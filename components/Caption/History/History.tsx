afd00919a646164098d18799c58fcb1bc444acf3
S44nt1ag0 <santiago.develop1@gmail.com>
2025-09-21 11:17:04 -0300

    added date and title to video history

--- components/Caption/History/History.tsx
+++ components/Caption/History/History.tsx
@@ -7,10 +7,12 @@ import { useRouter } from "next/navigation";
 import React, { useEffect } from "react";
 import { LuEye } from "react-icons/lu";
 import { LuHistory } from "react-icons/lu";
+import { LuCalendar } from "react-icons/lu";
 
 interface IHistory {
   id: string;
-  url: string;
+  title: string;
+  createdAt: Date;
 }
 
 export default function HistoryPage() {
@@ -41,51 +43,66 @@ export default function HistoryPage() {
   }, []);
 
   return (
-    <motion.div
-      className="my-10 max-h-[70vh] overflow-y-auto scroll-hidden"
-    >
+    <motion.div className="my-10 max-h-[70vh] overflow-y-auto scroll-hidden">
       <div className="my-7 w-full">
-      <div className="w-full flex gap-4 items-center">
-        <motion.div
-          initial={{ rotate: 360 }}
-          animate={{ rotate: 180 }}
-          transition={{ duration: 0.8, ease: "easeInOut" }}
-        >
-          <LuHistory className="text-4xl text-white/50" />
-        </motion.div>
+        <div className="w-full flex gap-4 items-center">
+          <motion.div
+            initial={{ rotate: 360 }}
+            animate={{ rotate: 180 }}
+            transition={{ duration: 0.8, ease: "easeInOut" }}
+          >
+            <LuHistory className="text-4xl text-white/50" />
+          </motion.div>
 
-        <div className="flex flex-col">
-        <h1 className="text-2xl font-bold text-white/80 ">
-          Latest Conversations
-        </h1>
-        <a className="text-white/60"> Track your History </a>
+          <div className="flex flex-col">
+            <h1 className="text-2xl font-bold text-white/80 ">
+              Latest Conversations
+            </h1>
+            <a className="text-white/60"> Track your History </a>
+          </div>
         </div>
-      </div>
       </div>
 
       <div className="w-full flex flex-col gap-5">
-      {history.map((item) => (
-        <motion.div
-          key={item.id}
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          exit={{ opacity: 0 }}
-          transition={{ duration: 0.4 }}
-        >
-          <Card
-            className="w-full p-3 flex shadow-2xl flex-row items-center justify-between"
+        {history.map((item) => (
+          <motion.div
+            key={item.id}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.4 }}
           >
-            <h3 className="text-white/60">{item.id}</h3>
-            <Button
-              variant="outline"
-              className="p-6 cursor-pointer min-w-[80px] shadow-2xl"
-              onClick={() => router.push(`/caption/${item.id}`)}
-            >
-              <LuEye className="text-8xl text-white/80" />
-            </Button>
-          </Card>
-        </motion.div>
-      ))}
+            <Card className="w-full p-4 flex shadow-2xl flex-row items-center justify-between">
+              <div>
+                <h3>
+                  {item.title && item.title.length > 40
+                    ? `${item.title.slice(0, 40)}...`
+                    : item.title || item.id}
+                </h3>
+              </div>
+
+              <div className="flex flex-row items-center gap-4">
+                <h3 className="flex flex-row items-center gap-2 text-white/60">
+                  <LuCalendar className="text-1xl text-white/80" />
+                  {new Date(item.createdAt).toLocaleString("pt-BR", {
+                    day: "2-digit",
+                    month: "2-digit",
+                    year: "numeric",
+                    hour: "2-digit",
+                    minute: "2-digit",
+                  })}
+                </h3>
+                <Button
+                  variant="outline"
+                  className="p-6 cursor-pointer rounded-2xl min-w-[70px] shadow-2xs"
+                  onClick={() => router.push(`/caption/${item.id}`)}
+                >
+                  <LuEye className="text-white/80" />
+                </Button>
+              </div>
+            </Card>
+          </motion.div>
+        ))}
       </div>
     </motion.div>
   );
